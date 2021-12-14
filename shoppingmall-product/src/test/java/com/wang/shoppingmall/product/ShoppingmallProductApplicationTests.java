c45ed894e0d8c7c5ec3630b4dad854103e745903
wanghongxiao <2763210788@qq.com>
2021-12-14 19:58:57 +0800

    项目初始结构创建

---
+++ shoppingmall-product/src/test/java/com/wang/shoppingmall/product/ShoppingmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.shoppingmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shoppingmall-ware/src/test/java/com/wang/shoppingmall/ware/ShoppingmallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.shoppingmall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
